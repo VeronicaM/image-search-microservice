6026c5c93058441cb075407b1102697c3fcda9b6
Mihai <mihaiveronica@ymail.com>
2017-11-10 11:52:13 +0200

    switch list to latest

--- api/index.js
+++ api/index.js
@@ -5,6 +5,6 @@ var controller = require('./images.controller');
 var router = express.Router();
 
 router.get('/imagesearch/:q', controller.search);
-router.get('/list/imagesearch', controller.listSearches);
+router.get('/latest/imagesearch', controller.listSearches);
 
 module.exports = router;
