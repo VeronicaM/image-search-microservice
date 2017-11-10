a61137d4ed68ec3ed71adfc7ffa1aa028a1e6b1a
Mihai <mihaiveronica@ymail.com>
2017-11-10 11:03:03 +0200

    add search images with pagination functionality

--- api/images.controller.js
+++ api/images.controller.js
@@ -1,8 +1,33 @@
 var images = require('./images.model');
+var request = require('request');
+
 
 exports.search = function(req, res) {
-    res.send("searching images");
+    var url = "https://www.googleapis.com/customsearch/v1?key=" +
+        process.env.API_KEY + "&cx=" +
+        process.env.ENGINE_ID + "&q=" + req.params.q + "&searchType=image&start=" + req.query.offset;
+    request.get(
+        url,
+        function(err, response, body) {
+            if (!response)
+                console.log('Error happened');
+            else {
+                res.send(formatResult(JSON.parse(body).items));
+            }
+
+        });
 };
+
+function formatResult(result) {
+    return result.map(function(item) {
+        return {
+            url: item.link,
+            snippet: item.snippet,
+            thumbnail: item.image.thumbnailLink,
+            context: item.image.contextLink
+        };
+    });
+}
 
 exports.listSearches = function(req, res) {
     // images.find({ userId: req.user._id }, function(err, clients) {
